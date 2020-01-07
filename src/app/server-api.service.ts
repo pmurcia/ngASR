d9ae9588c8402564023a3d1242be9b109b41c535
pmurcia <p.murcia.morilla@gmail.com>
2020-01-07 23:00:11 +0100

    Final angular template

--- src/app/server-api.service.ts
+++ src/app/server-api.service.ts
@@ -9,24 +9,24 @@ export class ServerApiService {
   constructor(private http: HttpClient) { }
 
   getStock(symbol: string) {
-    const result = this.http.get(`http://localhost:8080/practicaFinal/api/stock?symbol=${symbol}&ng=1`);
+    const result = this.http.get(`/api/stock?symbol=${symbol}&ng=1`);
     // result.toPromise().then(data => console.log(data));
     return result;
   }
 
   getNews(symbol: string) {
-    return this.http.get(`http://localhost:8080/practicaFinal/api/news?symbol=${symbol}&ng=1`);
+    return this.http.get(`/api/news?symbol=${symbol}&ng=1`);
   }
 
   getScore(symbol: string, newId: string) {
-    return this.http.get(`http://localhost:8080/practicaFinal/api/score?symbol=${symbol}&id=${newId}&ng=1`);
+    return this.http.get(`/api/score?symbol=${symbol}&id=${newId}&ng=1`);
   }
 
   getData(symbol: string) {
-    return this.http.get(`http://localhost:8080/practicaFinal/api/data?symbol=${symbol}&ng=1`);
+    return this.http.get(`/api/data?symbol=${symbol}&ng=1`);
   }
 
   getTranslation(symbol: string, newId: string, language: string) {
-    return this.http.get(`http://localhost:8080/practicaFinal/api/translate?symbol=${symbol}&id=${newId}&lang=${language}&ng=1`);
+    return this.http.get(`/api/translate?symbol=${symbol}&id=${newId}&lang=${language}&ng=1`);
   }
 }
